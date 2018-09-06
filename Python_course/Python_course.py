5523f1a8d8a2134b7afce748fdbd92275dc16658
yuelong_pc <wunnnn@hotmail.com>
2018-09-06 11:33:36 +0800

    数码管，彩色，正常了

--- Python_course/Python_course.py
+++ Python_course/Python_course.py
@@ -1,12 +1,17 @@
 '''
 数码管
 '''
-import turtle
+import turtle,time
+def drawGap():
+    turtle.penup()
+    turtle.fd(5)
 def drawLine(draw):
+    drawGap()
     turtle.pendown()if draw else turtle.penup()
     turtle.fd(40)
+    drawGap()
     turtle.right(90)
-def drawDigit(digit):
+def drawDigit(digit):#绘制1个
     drawLine(True) if digit in [2,3,4,5,6,7,8,9] else drawLine(False)
     drawLine(True) if digit in [0,1,3,4,5,6,7,8,9] else drawLine(False)
     drawLine(True) if digit in [0,2,3,5,6,8,9] else drawLine(False)
@@ -19,14 +24,26 @@ def drawDigit(digit):
     turtle.penup()
     turtle.fd(20)
 def drawDate(date):
+    turtle.pencolor("red")
     for i in date:
-        drawDigit(eval(i))
+        if i=='-':
+            turtle.write("年",font = ("Arial",18,"normal"))
+            turtle.pencolor("green")
+            turtle.fd(40)
+        elif i=="=":
+            turtle.write("月",font = ("Arial",18,"normal"))
+            turtle.pencolor("blue")
+            turtle.fd(40)
+        elif i=="+":
+            turtle.write("日",font = ("Arial",18,"normal"))
+        else:
+            drawDigit(eval(i))
 def main():
     turtle.setup(800,350,200,200)
     turtle.penup()
     turtle.fd(-300)
     turtle.pensize(5)
-    drawDate('20181010')
+    drawDate(time.strftime("%Y-%m=%d+", time.gmtime()))
     turtle.hideturtle()
     turtle.done()
 main()
