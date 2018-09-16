a0f5ce5a34fb986b6978b1f65b52bcbe60bb38f8
wunnnn <wunnnn@hotmail.com>
2018-09-16 16:35:09 +0800

    中文三国

--- Python_course/Python_course.py
+++ Python_course/Python_course.py
@@ -1,17 +1,24 @@
-#ClaHamletV1.py
-def getText():
-    txt=open("hamlet.txt","r").read()
-    txt=txt.lower()
-    for ch in '~!@#$%^&*()_-+={}[]:;,./<>?"\|':
-        txt=txt.replace(ch," ")
-    return txt
-hamletTxt=getText()
-words=hamletTxt.split()
+#CalThreeKingdomsV2.py
+import jieba
+
+txt =open("threekingdoms.txt","r",encoding="utf-8".read())
+exculdes={"将军","却说","荆州","二人","不可","不能","如此"}
+words=jieba.lcut(txt)
 counts={}
 for word in words:
-    counts[word]=counts.get(word,0)+1
+    if len(word)==1:
+        continue
+    elif word=="诸葛亮"or word=="孔明曰":
+        rword="孔明"
+else:
+    rword=word
+    counts[rword]=counts.get(reword,0)+1
+for word in exculdes:
+    del counts[word]
 items=list(counts.items())
-items.sort(key=lambda x:x[1],reverse=True)
+items=sorted(key=lambda x:x[1],reverse=True)
 for i in range(10):
     word,count=items[i]
-    print("{0:<10}{1:>5}".format(word,count))#对齐
+    print("{0:<10}{1:>5}".format(word,count))
+    
+
