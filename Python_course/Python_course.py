c7a42b6999250c92496a16ae875bbd6e52b146cc
wunnnn <wunnnn@hotmail.com>
2018-09-16 17:34:06 +0800

    改了错

--- Python_course/Python_course.py
+++ Python_course/Python_course.py
@@ -1,8 +1,7 @@
 #CalThreeKingdomsV2.py
 import jieba
-
-txt =open("threekingdoms.txt","r",encoding="utf-8".read())
-exculdes={"将军","却说","荆州","二人","不可","不能","如此"}
+txt =open("threekingdoms.txt","r",encoding='utf-8').read()
+exculdes={"将军","却说","荆州","二人","不可","不能","如此","这样"}
 words=jieba.lcut(txt)
 counts={}
 for word in words:
@@ -10,15 +9,14 @@ for word in words:
         continue
     elif word=="诸葛亮"or word=="孔明曰":
         rword="孔明"
-else:
-    rword=word
-    counts[rword]=counts.get(reword,0)+1
+    else:
+        rword=word
+    counts[rword]=counts.get(rword,0)+1
 for word in exculdes:
     del counts[word]
 items=list(counts.items())
-items=sorted(key=lambda x:x[1],reverse=True)
+items.sort(key=lambda x:x[1],reverse=True)
 for i in range(10):
     word,count=items[i]
     print("{0:<10}{1:>5}".format(word,count))
     
-
